f26856d2a90d0db4db8e13d02d9687261827d453
seet61 <seet_seet@mail.ru>
2018-08-01 14:13:05 +0300

    video 8

--- src/main/java/ru/seet61/spring/logger/CacheFileEventLogger.java
+++ src/main/java/ru/seet61/spring/logger/CacheFileEventLogger.java
@@ -3,11 +3,12 @@ package ru.seet61.spring.logger;
 import ru.seet61.spring.event.Event;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CacheFileEventLogger extends FileEventLogger{
     private int cacheSize;
-    private List<Event> cache;
+    private List<Event> cache = new ArrayList<Event>();
 
     public CacheFileEventLogger(String fileName) {
         super(fileName);
